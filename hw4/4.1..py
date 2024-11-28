6eb8898075c8b185e01f5ac00cc059618de1d4db
s_o_va <o.strygunova@gmail.com>
2024-11-28 14:17:13 +0200

    add new data

--- hw4/4.1..py
+++ hw4/4.1..py
@@ -1,7 +1,9 @@
-lst = [0, 1, 0, 12, 3, 8, 9, -1, 0, 5, 9, 2]
-result = []
+lst = [0, 1, 0, 12, 3, 9, 0, 0, 7, -2, 24]
+index = 0
 for i in range(len(lst)):
     if lst[i] != 0:
-        result.append(lst[i])
-result.extend([0] * (len(lst) - len(result)))
-print(result)
+        lst[index] = lst[i]
+        index += 1
+for i in range(index, len(lst)):
+    lst[i] = 0
+print(lst)
