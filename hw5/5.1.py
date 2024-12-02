01406405bc979f78f7e6003849a0121ec8a11568
s_o_va <o.strygunova@gmail.com>
2024-12-02 16:59:52 +0200

    add new data

--- hw5/5.1.py
+++ hw5/5.1.py
@@ -1,22 +1,23 @@
 import string
 import keyword
-def is_valid_variable(name):
-    if name in keyword.kwlist:
-        return False
-    if name[0].isdigit():
-        return False
-    if name.count('_') > 1:
-        return False
-    for char in name:
-        if char not in string.ascii_lowercase + string.digits + '_':
-            return False
-    if any(c.isupper() for c in name):
-        return False
-    return True
 while True:
-    user_input = input("Enter a string to check if it can be a variable name (or 'exit' to quit): ")
-    if user_input.lower() == "exit":
+    name = input("Enter a string to check if it can be a variable name (or 'exit' to quit): ").strip()
+    if name.lower() == "exit":
         print("Good Luck!")
         break
-    result = is_valid_variable(user_input)
-    print(result)
+    if name in keyword.kwlist:
+        print(False)
+        continue
+    if name[0].isdigit():
+        print(False)
+        continue
+    if any(char.isupper() for char in name):
+        print(False)
+        continue
+    if any(char in string.punctuation.replace("_", "") for char in name):
+        print(False)
+        continue
+    if name.count("__") > 1 :
+        print(False)
+        continue
+    print(True)
