80fd32f64bedd5721a714634d137e1246874d5c0
s_o_va <o.strygunova@gmail.com>
2024-12-09 17:59:50 +0200

    add new data

--- hw7/7.3.py
+++ hw7/7.3.py
@@ -1,9 +1,11 @@
 def second_index(text, some_str):
-    first_index = text.find(some_str)
-    if first_index == -1:
-        return None
-    second_index = text.find(some_str, first_index + 1)
-    return second_index if second_index != -1 else None
+    found_count = 0
+    for i in range(len(text)):
+        if text[i:i+len(some_str)] == some_str:
+            found_count += 1
+            if found_count == 2:
+                return i
+    return None
 assert second_index("sims", "s") == 3, 'Test1'
 assert second_index("find the river", "e") == 12, 'Test2'
 assert second_index("hi", "h") is None, 'Test3'
